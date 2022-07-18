6ee60f485c4795e803c40ec0421cacef13fdea5f
이지호 <cctv1998@naver.com>
2022-07-18 17:34:09 +0900

    refactor : 쓸모없는 구조분해 요소 삭제

--- src/components/adManagement/CardInner.tsx
+++ src/components/adManagement/CardInner.tsx
@@ -22,8 +22,7 @@ function CardInner({
   onChangeForm,
   onChangeReportForm,
 }: CardInnerProps) {
-  const { status, startDate, report, budget, adType, title } = ad;
-  console.log(form);
+  const { status, startDate, report, budget } = ad;
   return (
     <>
       <CardInnerStyled>
