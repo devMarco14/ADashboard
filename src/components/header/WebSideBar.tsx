271cab2dd94dbd528c79e96e01cc95871a7ecbec
이지호 <cctv1998@naver.com>
2022-07-19 01:48:53 +0900

    feat : 다크모드 적용 토글버튼 적용

--- src/components/header/WebSideBar.tsx
+++ src/components/header/WebSideBar.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import { ThemeContext } from 'libs/context/theme';
+import { FlexCenter } from 'libs/style/commonStyles';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import SideBarInner from './SideBarInner';
+import ToggleButton from './ToggleButton';
 
 function WebSideBar() {
+  const { ThemeMode, onToggleTheme } = useContext(ThemeContext);
+
   return (
     <SideBarBlock>
-      <BlankSpace />
+      <BlankSpace>
+        테마 변환
+        <ToggleButton isActive={ThemeMode} onToggle={onToggleTheme} />
+      </BlankSpace>
       <SideBarInner />
     </SideBarBlock>
   );
 }
-const BlankSpace = styled.div`
+const BlankSpace = styled(FlexCenter)`
   height: 45px;
+  gap: 8px;
 `;
 const SideBarBlock = styled.div`
   max-width: 250px;
@@ -19,6 +28,7 @@ const SideBarBlock = styled.div`
   display: block;
   position: relative;
   min-height: 100vh;
+  background-color: ${({ theme }) => theme.colors.backgroundColor};
   box-shadow: 2px 0px 4px 0px rgba(0, 0, 0, 0.22);
   ${({ theme }) => theme.media.small} {
     display: none;
