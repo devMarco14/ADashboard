71ab1fd8246aa130cce5d9b61d9ce8db7da5c54a
이지호 <cctv1998@naver.com>
2022-07-18 17:35:15 +0900

    feat : 필터 select 구현

--- src/pages/AdManagementPage.tsx
+++ src/pages/AdManagementPage.tsx
@@ -1,18 +1,31 @@
 import AdManagementArtcle from 'components/adManagement/AdManagementArticle';
 import useAdLoad from 'components/adManagement/hooks/useAdLoad';
+import useInput from 'hooks/useInput';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
 function AdManagementPage() {
   const [selectedAd, setSelectedAd] = useState<number | null>(null);
+  const [stateAD, onChangeStateAD] = useInput('');
+
   /* 추 후 수정되고 있는 섹션의 보더를 변경 */
-  const { adData, setDetectData } = useAdLoad();
+  const { adData, setDetectData } = useAdLoad(stateAD as string);
   return (
     <AdManagement>
       <AdManagementTitle>광고관리</AdManagementTitle>
       <AdManagementSection>
         <AdManagementHeader>
-          <div>드롭다운메뉴</div>
+          <div>
+            <ADFilterSelect
+              name="filter"
+              value={stateAD}
+              onChange={onChangeStateAD}
+            >
+              <option value="">전체</option>
+              <option value="active">진행중</option>
+              <option value="ended">종료</option>
+            </ADFilterSelect>
+          </div>
           <AdManagementButton>광고 만들기</AdManagementButton>
         </AdManagementHeader>
         <AdManagementArticle>
@@ -81,4 +94,14 @@ const AdManagementArticle = styled.article`
   }
 `;
 
+const ADFilterSelect = styled.select`
+  width: 100px;
+  height: 32px;
+  border-radius: 6px;
+  margin-left: 20px;
+  border: 1px solid ${({ theme }) => theme.colors.lightGrayColor};
+  ${({ theme }) => theme.media.small} {
+    margin-left: 0px;
+  }
+`;
 export default AdManagementPage;
