c613adb91d802853b68810c3e900397d6b4ebe4e
이지호 <cctv1998@naver.com>
2022-07-19 01:45:57 +0900

    refactor : theme 중복코드 삭제

--- src/libs/style/theme/darkTheme.ts
+++ src/libs/style/theme/darkTheme.ts
@@ -1,12 +1,4 @@
-export type FontSizes = typeof fontSizes;
-const fontSizes = {
-  xsmall: '10px',
-  small: '12px',
-  medium: '14px',
-  large: '16px',
-  xlarge: '18px',
-  xxlarge: '20px',
-};
+import { fontSizes, media } from './defaultTheme';
 
 export type Colors = typeof colors;
 
@@ -19,24 +11,6 @@ const colors = {
   lightGrayColor: '#d9d3c7',
   redColor: '#ea3a4a',
   greenColor: '#85da47',
-};
-
-// redColor: '#ea3a4a',
-// greenColor: '#85da47',
-export const mediaQuery = (maxWidth: number) => `
-@media (max-width: ${maxWidth}px)
-`;
-
-export type Media = typeof media;
-const media = {
-  xxlarge: mediaQuery(2020),
-  xlarge: mediaQuery(1640),
-  large: mediaQuery(1250),
-  medium: mediaQuery(1000),
-  small: mediaQuery(900),
-  xsmall: mediaQuery(640),
-  xxsmall: mediaQuery(480),
-  custom: mediaQuery,
 };
 
 const darkTheme = {
