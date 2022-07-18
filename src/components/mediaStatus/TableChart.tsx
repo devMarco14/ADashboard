831211cc58fd561330d65c75c7a3664460e02738
HyeonJu <hhjc017@gmail.com>
2022-07-18 14:04:12 +0900

    #8 refactor : 테이블 차트 리팩토링

--- src/components/mediaStatus/TableChart.tsx
+++ src/components/mediaStatus/TableChart.tsx
@@ -1,85 +1,52 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import styled from 'styled-components';
+import { CompanyType } from 'types/media-status';
 import useTransformedData from './hooks/useTransformedData';
 
 function TableChart() {
   const { getTableData } = useTransformedData();
   const tableData = getTableData();
+  const companeis: CompanyType[] = ['google', 'naver', 'kakao', 'facebook'];
+
+  const columnHeader = tableData.map((data) => (
+    <th className="col" scope="col">
+      {data.name}
+    </th>
+  ));
+
+  const dataByCompany = companeis.map((company) => (
+    <tr>
+      <th className="flatform row" scope="row">
+        {company}
+      </th>
+      {tableData.map((data) => (
+        <td>{Math.round(data[company])}</td>
+      ))}
+    </tr>
+  ));
+
+  const totalData = (
+    <tr>
+      <th className="total row">총계</th>
+      {tableData.map((data) => (
+        <td className="total">{Math.round(data.total)}</td>
+      ))}
+    </tr>
+  );
 
   return (
     <TableLayout>
       <Table>
-        <thead>
+        <thead aria-label="플랫폼 기준 데이터">
           <tr>
-            <th className="col flatform" aria-label="플랫폼" />
-            <th className="col" scope="col">
-              광고비
-            </th>
-            <th className="col" scope="col">
-              매출
-            </th>
-            <th className="col" scope="col">
-              ROAS
-            </th>
-            <th className="col" scope="col">
-              노출수
-            </th>
-            <th className="col" scope="col">
-              클릭수
-            </th>
-            <th className="col" scope="col">
-              클릭률 <br /> (CTR)
-            </th>
-            <th className="col" scope="col">
-              전환율 <br /> (CVR)
-            </th>
-            <th className="col" scope="col">
-              클릭당 비용 <br /> (CPC)
-            </th>
-            <th className="col" scope="col">
-              전환당 비용 <br /> (CPA)
-            </th>
+            <th className="col flatform" />
+            {columnHeader}
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <th className="flatform row" scope="row">
-              페이스북
-            </th>
-            {tableData.map((data) => (
-              <td>{Math.round(data.facebook)}</td>
-            ))}
-          </tr>
-          <tr>
-            <th className="flatform row" scope="row">
-              네이버
-            </th>
-            {tableData.map((data) => (
-              <td>{Math.round(data.naver)}</td>
-            ))}
-          </tr>
-          <tr>
-            <th className="flatform row" scope="row">
-              구글
-            </th>
-            {tableData.map((data) => (
-              <td>{Math.round(data.google)}</td>
-            ))}
-          </tr>
-          <tr>
-            <th className="flatform row" scope="row">
-              카카오
-            </th>
-            {tableData.map((data) => (
-              <td>{Math.round(data.kakao)}</td>
-            ))}
-          </tr>
-          <tr>
-            <th className="total row">총계</th>
-            {tableData.map((data) => (
-              <td className="total">{Math.round(data.total)}</td>
-            ))}
-          </tr>
+        <tbody aria-label="플랫폼">
+          {dataByCompany}
+          {totalData}
         </tbody>
       </Table>
     </TableLayout>
@@ -97,6 +64,21 @@ const TableLayout = styled.section`
   background-color: ${({ theme }) => theme.colors.whiteColor};
   border-radius: 8px;
   font-weight: 300;
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    height: 5%;
+    background: ${({ theme }) => theme.colors.lightGrayColor};
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: ${({ theme }) => theme.colors.backgroundColor};
+    border-radius: 10px;
+  }
 `;
 
 const Table = styled.table`
@@ -120,5 +102,6 @@ const Table = styled.table`
   & .total,
   & .total {
     color: ${({ theme }) => theme.colors.blueColor};
+    font-weight: 700;
   }
 `;
